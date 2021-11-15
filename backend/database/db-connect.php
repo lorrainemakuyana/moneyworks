f847a32dbdff801d0a3c3c0727826872343d6035
Lorraine Makuyana <58143262+lorrainemakuyana@users.noreply.github.com>
2021-11-15 23:26:11 +0000

    Add backend

---
+++ backend/database/db-connect.php
@@ -0,0 +1,73 @@
+<?php
+
+require('db_cred.php');
+
+/**
+ *@author David Sampah
+ *@version 1.1
+ */
+class db_connection
+{
+	//properties
+	public $db = null;
+	public $results = null;
+
+	//connect
+	/**
+	*Database connection
+	*@return bolean
+	**/
+	function db_connect(){
+		//connection
+		$this->db = mysqli_connect(SERVER,USERNAME,PASSWD,DATABASE);
+		//test the connection
+		if (mysqli_connect_errno()) {
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	//execute a query
+	/**
+	*Query the Database
+	*@param takes a connection and sql query
+	*@return bolean
+	**/
+	function db_query($sqlQuery){
+		if (!$this->db_connect()) {
+			return false;
+		}
+		elseif ($this->db==null) {
+			return false;
+		}
+
+		//run query
+		$this->results = mysqli_query($this->db,$sqlQuery);
+		if ($this->results == false) {
+			return false;
+		}else{
+			return true;
+		}
+
+	}
+
+	//fetch data
+	/**
+	*get select data
+	*@return a record
+	**/
+	function db_fetch(){
+		//check if result was set
+		if ($this->results == null) {
+			return false;
+		}
+		elseif ($this->results == false) {
+			return false;
+		}
+		//return a record
+		return mysqli_fetch_assoc($this->results);
+
+	}
+}
+?>
---
+++ backend/database/db-credentials.php
@@ -0,0 +1,11 @@
+<?php
+
+    define("DATABASE", "money_works");
+
+    define("SERVER", "localhost");
+
+    define("USERNAME", "root");
+
+    define("PASSWD", "12345678");
+
+?>
